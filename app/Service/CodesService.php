d6e5e7ffda2b8dc418c42883899214a5c1b286b4
VG-Electronics <biuro@vgen.pl>
2021-03-04 16:33:44 +0100

    wyszukiwanie poprawione na bardziej uniwersalne

--- app/Service/CodesService.php
+++ app/Service/CodesService.php
@@ -7,13 +7,52 @@ use Illuminate\Http\Request;
 
 class CodesService{
 
+    private $directSearch = [
+        'code'
+    ];
+
+    private $likeSearch = [
+        'name'
+    ];
+
+    private $phraseSearchable = [
+        'description'
+    ];
+
     public function searchByRequest( Request $request ){
+        $phrase = strtolower($request->phrase);
+        $filters = array_filter( $request->all() );
  
-        return Code::where('name', 'LIKE', '%'.$request->name.'%')
-            ->when($code = $request->code, function($query) use($code){
-                return $query->where('code', $code);
+        return Code::where(function($query) use($filters){
+                
+                foreach( $filters as $field => $value){
+                    
+                    if( $this->isDirectSearch($field) )
+                        $query = $query->where($field, $value);
+                    elseif( $this->isLikeSearch($field) )
+                        $query = $query->where($field, 'LIKE', '%'.$value.'%');
+                }
+
+                return $query;
+            })
+            ->when($phrase, function($query) use($phrase){
+                if( in_array($phrase, ['aktywny', 'nieaktywny']) )
+                    $query = $query->where('active', $phrase == 'aktywny');
+
+                return $query->orWhere(function($query) use($phrase){
+                    foreach($this->phraseSearchable as $field)
+                        return $query->orWhere($field, 'LIKE', '%'.$phrase.'%');
+                });
             })
             ->WhereDateBetween('created_at', $request->date_from, $request->date_to)
             ->get();
+    }
+
+    private function isDirectSearch( $field ){
+        return in_array($field, $this->directSearch);
+    }
+
+    private function isLikeSearch( $field ){
+        return in_array($field, $this->likeSearch);
     }
 }
